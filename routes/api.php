a086580df596dd34daf27fb1dc27ae0e99a5a3e4
jurapro <jurapro@yandex.ru>
2021-09-28 20:31:38 -0700

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('login',[UserController::class, 'login']);
+Route::post('signup',[UserController::class, 'signup']);
+Route::get('products',[ProductController::class, 'index']);
+Route::get('logout',[UserController::class, 'logout'])->middleware('auth:api');
